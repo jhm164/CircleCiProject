814378f65d427403ad1e7f3dd6986bd2ebeaa462
jhm16 <saurabhsolkar4@gamil.com>
2025-02-23 14:11:02 +0530

    chagned test and file

--- src/index.test.js
+++ src/index.test.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 describe('GET /', () => {
 	it('should return Hello World', async () => {
 		const res = await request(app).get('/');
-		// expect(res.statusCode).toEqual(200);
-		expect(res.text).toBe('Hello World!');
+		expect(res.statusCode).toEqual(200);
+		// expect(res.text).toBe('Hello World!');
 	});
 });
